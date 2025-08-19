77bb0c5e910a7f7bb69983509969260004092afd
Prateek Potdar <93429226+PRATEEKA001@users.noreply.github.com>
2025-08-19 17:46:20 +0800

    Create About.js

---
+++ pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This is a demo React application.</p>
+    </div>
+  );
+}
+
+export default About;
